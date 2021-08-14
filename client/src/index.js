f3cda914ec331d340b08c8a46603d8893e4f7c03
Unknown <linyanz1@student.unimelb.edu.au>
2021-08-14 23:57:39 +1000

    Integrate Auth0 to frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-8p7irqly.us.auth0.com"
+      clientId="nQuq0RRvq1qHJm71fVywenxJndfhMvAY"
+      redirectUri={window.location.origin}
+      audience="localhost:3000/api/"
+      scope="read:posts"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
