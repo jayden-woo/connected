e1568601d66537b18b696f922df63ab1fb2ddf7b
Unknown <linyanz1@student.unimelb.edu.au>
2021-08-17 16:49:41 +1000

    Fixing directory issue

--- server/startup/morgan.js
+++ server/startup/morgan.js
@@ -7,9 +7,9 @@ module.exports = function (app) {
     app.use(morgan("tiny"));
     debug("Morgan enabled...");
   } else if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static(path.join(__dirname, "../../client/build")));
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+      res.sendFile(path.join(__dirname + "/../../client/build/index.html"));
     });
   }
 };
