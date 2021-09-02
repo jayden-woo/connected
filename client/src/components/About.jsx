9c7d67158a2ff271ed82ee20a5dfb08cb1000095
Chen Jiang <chenjiang@student.unimelb.edu.au>
2021-09-02 17:07:23 +1000

    02/09 title amending: ABOUT US

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => (
     <div>
         <Image className="center-headerImage" src={headerImage} alt="about header Image" />
         <div> <br /> <br />  <br /> </div>
-        <h1 style={{textAlign: 'center' , color: '#000766'}} > About </h1>
+        <h1 style={{textAlign: 'center' , color: '#000766'}} > ABOUT US </h1>
         <div> <br /> <br /> </div>
         <p style={{textAlign: 'center' , color: '#000766'}}  > 
             2222222222222222223333333333333444444jjjjjjjj2222222222222222222222222222333333333333333333333333444444jjjjjjjjjj2222222222222222222222222222333333333333333333333333444444jjjjjjjjjjjjjjshhahahsdhahsdhashdhashdahjs
