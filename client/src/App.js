611159eb80c19bf7615870e46c7e8c8c9fd521b0
Chen Jiang <chenjiang@student.unimelb.edu.au>
2021-09-06 12:34:56 +1000

    06/09 new about

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Layout } from 'antd';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from'./components/Footer'
@@ -9,14 +8,14 @@ import About from './components/About';
 function App() {
 	return (
 		<div> 
-			<Layout>
-				<Navigation id="top" />
-				<BrowserRouter>
-					<Switch>
-						<Route path="/create-survey" component={SurveyEditor} />
-					</Switch>
-				</BrowserRouter>
-			</Layout>
+			<Navigation id="top" />
+			<BrowserRouter>
+				<Switch>
+					<Route path="/create-survey" component={SurveyEditor} />
+					<Route path="/about" component={About} />
+				</Switch>
+			</BrowserRouter>
+			<Footer/>
 		</div>
 	);
 };
