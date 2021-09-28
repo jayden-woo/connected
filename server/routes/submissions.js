d5db903e0b2398abf0e931aa6108a0a3160a8926
Unknown <linyanz1@student.unimelb.edu.au>
2021-09-28 15:19:47 +1000

    revert back

--- server/routes/submissions.js
+++ server/routes/submissions.js
@@ -1,13 +1,26 @@
+// const express = require("express");
+// const router = express.Router();
+// const validateObjectId = require("../middleware/validateObjectId");
+// const controller = require("../controllers/submissions");
+// const { checkJwt, checkScopes } = require("../startup/auth0");
+
+// router.get("/", checkJwt, checkScopes, (req, res) =>
+// 	controller.getAllSubmissions(req, res),
+// );
+// router.get("/:id", checkJwt, checkScopes, validateObjectId, (req, res) =>
+// 	controller.getSubmissionByID(req, res),
+// );
+// router.post("/", (req, res) => controller.addSubmission(req, res));
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const validateObjectId = require("../middleware/validateObjectId");
 const controller = require("../controllers/submissions");
-const { checkJwt, checkScopes } = require("../startup/auth0");
 
-router.get("/", checkJwt, checkScopes, (req, res) =>
-	controller.getAllSubmissions(req, res),
-);
-router.get("/:id", checkJwt, checkScopes, validateObjectId, (req, res) =>
+router.get("/", (req, res) => controller.getAllSubmissions(req, res));
+router.get("/:id", validateObjectId, (req, res) =>
 	controller.getSubmissionByID(req, res),
 );
 router.post("/", (req, res) => controller.addSubmission(req, res));
