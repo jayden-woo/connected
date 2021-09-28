a9f82d6c2c698b188b424b9317bb0d8eb6200fba
Unknown <linyanz1@student.unimelb.edu.au>
2021-09-28 15:21:06 +1000

    test auth0

--- server/startup/auth0.js
+++ server/startup/auth0.js
@@ -1,33 +1,11 @@
-// const jwt = require("express-jwt");
-// const jwtAuthz = require("express-jwt-authz");
-// const jwksRsa = require("jwks-rsa");
-
-// const audience =
-// 	process.env.NODE_ENV === "production"
-// 		? "https://it-project-connected-api.herokuapp.com/"
-// 		: "localhost:3000/api/";
-
-// const checkJwt = jwt({
-// 	secret: jwksRsa.expressJwtSecret({
-// 		cache: true,
-// 		rateLimit: true,
-// 		jwksRequestsPerMinute: 5,
-// 		jwksUri: `https://dev-8p7irqly.us.auth0.com/.well-known/jwks.json`,
-// 	}),
-
-// 	audience: "localhost:3000/api/",
-// 	issuer: [`https://dev-8p7irqly.us.auth0.com/`],
-// 	algorithms: ["RS256"],
-// });
-
-// const checkScopes = jwtAuthz(["read:submission", "edit:survey"]);
-
-// exports.checkJwt = checkJwt;
-// exports.checkScopes = checkScopes;
-
 const jwt = require("express-jwt");
 const jwtAuthz = require("express-jwt-authz");
 const jwksRsa = require("jwks-rsa");
+
+const audience =
+	process.env.NODE_ENV === "production"
+		? "https://it-project-connected-api.herokuapp.com/"
+		: "localhost:3000/api/";
 
 const checkJwt = jwt({
 	secret: jwksRsa.expressJwtSecret({
@@ -42,7 +20,29 @@ const checkJwt = jwt({
 	algorithms: ["RS256"],
 });
 
-const checkScopes = jwtAuthz(["read:posts"]);
+const checkScopes = jwtAuthz(["read:submission", "edit:survey"]);
 
 exports.checkJwt = checkJwt;
 exports.checkScopes = checkScopes;
+
+// const jwt = require("express-jwt");
+// const jwtAuthz = require("express-jwt-authz");
+// const jwksRsa = require("jwks-rsa");
+
+// const checkJwt = jwt({
+// 	secret: jwksRsa.expressJwtSecret({
+// 		cache: true,
+// 		rateLimit: true,
+// 		jwksRequestsPerMinute: 5,
+// 		jwksUri: `https://dev-8p7irqly.us.auth0.com/.well-known/jwks.json`,
+// 	}),
+
+// 	audience: "localhost:3000/api/",
+// 	issuer: [`https://dev-8p7irqly.us.auth0.com/`],
+// 	algorithms: ["RS256"],
+// });
+
+// const checkScopes = jwtAuthz(["read:posts"]);
+
+// exports.checkJwt = checkJwt;
+// exports.checkScopes = checkScopes;
