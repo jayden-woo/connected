37f6958aea33525dafba2e8f4d4df64458a2a333
Unknown <linyanz1@student.unimelb.edu.au>
2021-09-28 15:51:14 +1000

    asdf

--- server/startup/auth0.js
+++ server/startup/auth0.js
@@ -1,33 +1,11 @@
-// const jwt = require("express-jwt");
-// const jwtAuthz = require("express-jwt-authz");
-// const jwksRsa = require("jwks-rsa");
-
-// const audience = "localhost:3000/api/";
-// // const audience =
-// // 	process.env.NODE_ENV === "production"
-// // 		? "https://it-project-connected-api.herokuapp.com/"
-// // 		: "localhost:3000/api/";
-
-// const checkJwt = jwt({
-// 	secret: jwksRsa.expressJwtSecret({
-// 		cache: true,
-// 		rateLimit: true,
-// 		jwksRequestsPerMinute: 5,
-// 		jwksUri: `https://dev-8p7irqly.us.auth0.com/.well-known/jwks.json`,
-// 	}),
-
-// 	audience,
-// 	issuer: [`https://dev-8p7irqly.us.auth0.com/`],
-// 	algorithms: ["RS256"],
-// });
-
-// const checkScopes = jwtAuthz(["read:submission", "edit:survey"]);
-
-// module.exports = { checkJwt, checkScopes };
-
 const jwt = require("express-jwt");
 const jwtAuthz = require("express-jwt-authz");
 const jwksRsa = require("jwks-rsa");
+
+const audience =
+	process.env.NODE_ENV === "production"
+		? "https://it-project-connected-api.herokuapp.com/"
+		: "localhost:3000/api/";
 
 const checkJwt = jwt({
 	secret: jwksRsa.expressJwtSecret({
@@ -37,12 +15,11 @@ const checkJwt = jwt({
 		jwksUri: `https://dev-8p7irqly.us.auth0.com/.well-known/jwks.json`,
 	}),
 
-	audience: "localhost:3000/api/",
+	audience,
 	issuer: [`https://dev-8p7irqly.us.auth0.com/`],
 	algorithms: ["RS256"],
 });
 
-const checkScopes = jwtAuthz(["read:posts"]);
+const checkScopes = jwtAuthz(["read:submission", "edit:survey"]);
 
-exports.checkJwt = checkJwt;
-exports.checkScopes = checkScopes;
+module.exports = { checkJwt, checkScopes };
--- server/startup/routes.js
+++ server/startup/routes.js
@@ -3,28 +3,9 @@ const posts = require("../routes/posts");
 const surveys = require("../routes/surveys");
 const submissions = require("../routes/submissions");
 
-const jwt = require("express-jwt");
-const jwtAuthz = require("express-jwt-authz");
-const jwksRsa = require("jwks-rsa");
-
-const checkJwt = jwt({
-	secret: jwksRsa.expressJwtSecret({
-		cache: true,
-		rateLimit: true,
-		jwksRequestsPerMinute: 5,
-		jwksUri: `https://dev-8p7irqly.us.auth0.com/.well-known/jwks.json`,
-	}),
-
-	audience: "https://it-project-connected-api.herokuapp.com/",
-	issuer: [`https://dev-8p7irqly.us.auth0.com/`],
-	algorithms: ["RS256"],
-});
-
 module.exports = function (app) {
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
-
-	app.get("/api/test", (req, res) => res.send("test"));
 
 	app.use("/api/posts", posts);
 	app.use("/api/surveys", surveys);
