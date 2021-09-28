5bc069485734989fc586b9ac0ff32ee2ae02de31
Unknown <linyanz1@student.unimelb.edu.au>
2021-09-28 15:27:59 +1000

    check   scope function

--- server/routes/surveys.js
+++ server/routes/surveys.js
@@ -2,16 +2,14 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/surveys");
 const validateObjectId = require("../middleware/validateObjectId");
-const { checkJwt, checkScopes } = require("../startup/auth0");
+const { checkJwt } = require("../startup/auth0");
 
 router.get("/", (req, res) => controller.getAllSurveys(req, res));
 router.get("/:id", validateObjectId, (req, res) =>
 	controller.getSurveyByID(req, res),
 );
-router.post("/", checkJwt, checkScopes, (req, res) =>
-	controller.addSurvey(req, res),
-);
-router.put("/:id", checkJwt, checkScopes, validateObjectId, (req, res) =>
+router.post("/", checkJwt, (req, res) => controller.addSurvey(req, res));
+router.put("/:id", checkJwt, validateObjectId, (req, res) =>
 	controller.updateSurvey(req, res),
 );
 
