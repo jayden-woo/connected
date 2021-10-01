8281c11d272da70dbf467da9663ae352417e4315
Xinyue Zhang <zxz1@student.unimelb.edu.au>
2021-10-01 17:50:01 +1000

    DB settup update (#50)
    Authored-by: Jayden <80225713+jayden-woo@users.noreply.github.com>

--- server/startup/db.js
+++ server/startup/db.js
@@ -3,12 +3,29 @@ const mongoose = require("mongoose");
 const debug = require("debug")("app:db");
 
 module.exports = function () {
+  // CONNECTION_STRING =
+  //   "mongodb+srv://<username>:<password>@team-connected.94ydr.mongodb.net/CRM?retryWrites=true&w=majority";
+  // MONGO_URL = CONNECTION_STRING.replace(
+  //   "<username>",
+  //   process.env.MONGO_USERNAME
+  // ).replace("<password>", process.env.MONGO_PASSWORD);
+
   CONNECTION_STRING =
-    "mongodb+srv://<username>:<password>@team-connected.94ydr.mongodb.net/CRM?retryWrites=true&w=majority";
-  MONGO_URL = CONNECTION_STRING.replace(
+    "mongodb+srv://<username>:<password>@<db>/CRM?retryWrites=true&w=majority";
+    // mongodb+srv://IT-PROJECT:<password>@<team-connected.94ydr.mongodb.net>/myFirstDatabase?retryWrites=true&w=majority
+    // mongodb+srv://IT-PROJECT:<password>@<it-project-prod.qth1n.mongodb.net>/myFirstDatabase?retryWrites=true&w=majority
+  MONGO_URL = CONNECTION_STRING
+  .replace(
     "<username>",
     process.env.MONGO_USERNAME
-  ).replace("<password>", process.env.MONGO_PASSWORD);
+  ).replace(
+    "<password>",
+    process.env.MONGO_PASSWORD
+  ).replace(
+    "<db>",
+    process.env.MONGO_DB
+  );
+  console.log(CONNECTION_STRING);
 
   mongoose.connect(MONGO_URL || "mongodb://localhost", {
     useNewUrlParser: true,
