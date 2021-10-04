bfb81e0ca83cd003c8a13dbf4c2ab72f5c083212
jayden-woo <woojw@student.unimelb.edu.au>
2021-10-04 18:01:40 +1100

    Fix css for post page in small phone screen

--- client/src/components/post/PostStats.jsx
+++ client/src/components/post/PostStats.jsx
@@ -1,44 +1,52 @@
 import { Col, Container, Row } from "react-bootstrap";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import MediaQuery from "react-responsive";
+// import MediaQuery from "react-responsive";
 import styled from "styled-components";
 
 const StyledContainer = styled(Container)`
   border: 1px solid var(--color-primary);
   border-radius: 0.75rem;
-  margin: 1rem 10%;
+  margin: 0.5rem 0;
   font-size: 1.1rem;
-  max-width: 80%;
+  max-width: 100%;
+  @media (min-width: 768px) {
+    margin: 1rem 10%;
+    max-width: 80%;
+  }
 `;
 
 const PostStats = ({ views, comments, solved }) => (
   <StyledContainer>
-    <Row className="p-3 pt-4 align-items-center">
+    <Row className="px-1 py-2 p-md-3 pt-4 align-items-center">
       <Col>
         <FontAwesomeIcon icon="eye" size="lg" color="var(--color-primary)" />
         &nbsp;{` ${views}`}
-        <MediaQuery minWidth={768}>{views > 1 ? ` Views` : ` View`}</MediaQuery>
+        {views > 1 ? ` Views` : ` View`}
+        {/* <MediaQuery minWidth={768}>{views > 1 ? ` Views` : ` View`}</MediaQuery> */}
       </Col>
     </Row>
-    <Row className="p-3 align-items-center">
+    <Row className="px-1 py-2 p-md-3 align-items-center">
       <Col>
         <FontAwesomeIcon icon="comments" size="lg" color="var(--color-primary)" />
         &nbsp;{` ${comments}`}
-        <MediaQuery minWidth={768}>{comments > 1 ? ` Comments` : ` Comment`}</MediaQuery>
+        {comments > 1 ? ` Comments` : ` Comment`}
+        {/* <MediaQuery minWidth={768}>{comments > 1 ? ` Comments` : ` Comment`}</MediaQuery> */}
       </Col>
     </Row>
-    <Row className="p-3 pb-4 align-items-center">
+    <Row className="px-1 py-2 p-md-3 pb-4 align-items-center">
       <Col>
         {solved ? (
           <>
             <FontAwesomeIcon icon="check-circle" size="lg" color="var(--bs-success)" />
-            <MediaQuery minWidth={768}>&nbsp;{` Solved`}</MediaQuery>
+            &nbsp;{` Solved`}
+            {/* <MediaQuery minWidth={768}>&nbsp;{` Solved`}</MediaQuery> */}
           </>
         ) : (
           <>
             <FontAwesomeIcon icon="times-circle" size="lg" color="var(--bs-danger)" />
-            <MediaQuery minWidth={768}>&nbsp;{` Unsolved`}</MediaQuery>
+            &nbsp;{` Unsolved`}
+            {/* <MediaQuery minWidth={768}>&nbsp;{` Unsolved`}</MediaQuery> */}
           </>
         )}
       </Col>
