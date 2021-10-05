d2cf2b450c5d6d8ed1633f39a40551caf90f2bf1
Zhihui Chen <zhihuic2@student.unimelb.edu.au>
2021-10-05 10:24:01 +0800

    merge

--- server/startup/cors.js
+++ server/startup/cors.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const whitelist = [
 	'https://it-project-connected.herokuapp.com',
 	'http://localhost:5000',
-	'https://it-project-connected-staging.herokuapp.com', 
-	'https://it-project-connected-dev.herokuapp.com', 
+	'https://it-project-connected-staging.herokuapp.com/', 
+	'https://it-project-connected-dev.herokuapp.com/', 
 	'https://it-project-connected-br-releas.herokuapp.com'
 ];
 
