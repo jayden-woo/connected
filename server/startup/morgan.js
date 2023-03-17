1f43b7bf8a5f1583cdcf4a0f5ed75e652c50e125
jayden-woo <juncheng1301@gmail.com>
2023-03-18 06:19:55 +1100

    Clean up server dependencies

--- server/startup/morgan.js
+++ server/startup/morgan.js
@@ -1,16 +1,16 @@
-const path = require("path");
+// const path = require("path");
 const morgan = require("morgan");
-const express = require("express");
+// const express = require("express");
 const debug = require("debug")("app:startup");
 
 module.exports = function (app) {
   if (app.get("env") === "development") {
     app.use(morgan("tiny"));
     debug("Morgan enabled...");
-  // } else if (process.env.NODE_ENV === "production") {
-  //   app.use(express.static(path.join(__dirname, "../../client/build")));
-  //   app.get("*", (req, res) => {
-  //     res.sendFile(path.join(__dirname + "/../../client/build/index.html"));
-  //   });
+    // } else if (process.env.NODE_ENV === "production") {
+    //   app.use(express.static(path.join(__dirname, "../../client/build")));
+    //   app.get("*", (req, res) => {
+    //     res.sendFile(path.join(__dirname + "/../../client/build/index.html"));
+    //   });
   }
 };
